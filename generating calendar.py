4c27f2707e06a7289c32d2ad654b3cec68283fc0
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-08-31 11:34:54 +0530

    Add files via upload

---
+++ generating calendar.py
@@ -0,0 +1,3 @@
+import calendar
+n=int(input("Enter the year for which you need to generate calendar:"))
+print(calendar.calendar(n))#generating the whole year calendar using inbuilt function calendar
